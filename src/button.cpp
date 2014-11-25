a3a500f81c0bc68c9f47436ab3bc18f1e3c4abfe
Dominic Bowden <tedle@blon.se>
2014-11-25 18:27:15 +0000

    Fix float to int warning

--- src/button.cpp
+++ src/button.cpp
@@ -11,7 +11,7 @@ Button::Button(int x, int y, int width, int height, const char* label, Manager* 
                static_cast<float>(y),
                static_cast<float>(width),
                static_cast<float>(height));
-    label_ = std::unique_ptr<Label>(new Label(pos_.x, pos_.y, label, parent_manager, parent_window));
+    label_ = std::unique_ptr<Label>(new Label(x, y, label, parent_manager, parent_window));
     gui_ = parent_manager;
     parent_ = parent_window;
 }
