63ee468d7289bdc54c79891f835d7fd57cd2f207
Dominic Bowden <tedle@blon.se>
2014-12-06 19:00:44 +0000

    .gitignore OOPSIEEE

---
+++ include/debug/logger.h
@@ -0,0 +1,37 @@
+#ifndef BLONSTECH_DEBUG_LOGGER_H_
+#define BLONSTECH_DEBUG_LOGGER_H_
+
+// Includes
+#include <memory>
+#include <string>
+
+namespace blons
+{
+class LoggerAPI
+{
+public:
+    enum Level {
+        DEBUG = 1,
+        INFO = 2,
+        WARN = 3,
+        FATAL = 4,
+        NONE = 5
+    };
+
+    void Debug(const char* fmt, ...);
+    void Info(const char* fmt, ...);
+    void Warn(const char* fmt, ...);
+    void Fatal(const char* fmt, ...);
+
+protected:
+    virtual void out(std::string msg, Level log_level)=0;
+    Level log_level_ = Level::NONE;
+
+private:
+    std::string format(const char* fmt, va_list args);
+};
+
+extern std::unique_ptr<LoggerAPI> g_log;
+} // namespace blons
+
+#endif
---
+++ include/debug/loggeride.h
@@ -0,0 +1,19 @@
+#ifndef BLONSTECH_DEBUG_LOGGERIDE_H_
+#define BLONSTECH_DEBUG_LOGGERIDE_H_
+
+//Local Includes
+#include "logger.h"
+
+namespace blons
+{
+class LoggerIDE : public LoggerAPI
+{
+public:
+    LoggerIDE(Level log_level);
+
+private:
+    void out(std::string msg, Level log_level);
+};
+} // namespace blons
+
+#endif
