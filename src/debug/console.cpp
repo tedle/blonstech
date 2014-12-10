26d773a4cc243dd2679427853b0a18a26b46e8fd
Dominic Bowden <tedle@blon.se>
2014-12-10 09:02:26 +0000

    Basic command parsing

--- src/debug/console.cpp
+++ src/debug/console.cpp
@@ -10,6 +10,150 @@ struct ConsoleState
 {
     std::vector<PrintCallback> print_callbacks;
 } g_state;
+
+struct ConsoleArg
+{
+    enum ValueType
+    {
+        NONE,
+        FUNCTION,
+        STRING,
+        INT,
+        FLOAT
+    } type;
+    std::string value;
+};
+
+// TODO: Move parsing functions to separate translation unit
+struct ParseState
+{
+    std::vector<ConsoleArg> args;
+    ConsoleArg current_arg = ConsoleArg{ ConsoleArg::FUNCTION, "" };
+    unsigned char current_letter;
+    bool open_string = false;
+};
+
+void ParseSpace(ParseState* state)
+{
+    if (!state->open_string)
+    {
+        if (state->current_arg.type == ConsoleArg::NONE ||
+            state->current_arg.value.length() == 0)
+        {
+            throw "Unexpected space";
+        }
+        state->args.push_back(state->current_arg);
+        state->current_arg.type = ConsoleArg::NONE;
+        state->current_arg.value.clear();
+    }
+    else
+    {
+        state->current_arg.value += ' ';
+    }
+}
+
+void ParseNumber(ParseState* state)
+{
+    if (state->current_arg.type == ConsoleArg::NONE)
+    {
+        state->current_arg.type = ConsoleArg::INT;
+    }
+    else if (state->current_letter == '-' &&
+             state->current_arg.type != ConsoleArg::FUNCTION)
+    {
+        state->current_arg.type = ConsoleArg::STRING;
+    }
+    state->current_arg.value += state->current_letter;
+}
+
+void ParseFloat(ParseState* state)
+{
+    if (state->current_arg.type == ConsoleArg::INT)
+    {
+        state->current_arg.type = ConsoleArg::FLOAT;
+    }
+    state->current_arg.value += state->current_letter;
+}
+
+void ParseQuote(ParseState* state)
+{
+    if (state->current_arg.type != ConsoleArg::NONE &&
+        state->open_string == false)
+    {
+        throw "Unexpected quote";
+    }
+    if (!state->open_string)
+    {
+        state->current_arg.type = ConsoleArg::STRING;
+    }
+    else
+    {
+        state->args.push_back(state->current_arg);
+        state->current_arg.type = ConsoleArg::NONE;
+        state->current_arg.value.clear();
+    }
+    state->open_string = !state->open_string;
+}
+
+void ParseString(ParseState* state)
+{
+    if (state->current_arg.type != ConsoleArg::FUNCTION)
+    {
+        state->current_arg.type = ConsoleArg::STRING;
+    }
+    state->current_arg.value += state->current_letter;
+}
+
+void ParseFinish(ParseState* state)
+{
+    if (state->open_string)
+    {
+        throw "Missing close quote";
+    }
+    if (state->current_arg.type != ConsoleArg::NONE &&
+        state->current_arg.value.length() != 0)
+    {
+        state->args.push_back(state->current_arg);
+    }
+}
+
+std::vector<ConsoleArg> ParseArgs(const std::string& command)
+{
+    ParseState state;
+    for (const auto& c : command)
+    {
+        state.current_letter = c;
+
+        if (c == ' ')
+        {
+            ParseSpace(&state);
+        }
+        else if ((c >= '0' && c <= '9') ||
+                  c == '-')
+        {
+            ParseNumber(&state);
+        }
+        else if (c == '.')
+        {
+            ParseFloat(&state);
+        }
+        else if (c == '"')
+        {
+            ParseQuote(&state);
+        }
+        else if (c >= 32 && c <= 126)
+        {
+            ParseString(&state);
+        }
+        else
+        {
+            throw "Unexpected character";
+        }
+    }
+    ParseFinish(&state);
+
+    return state.args;
+}
 } // namespace
 
 namespace blons
@@ -19,6 +163,43 @@ namespace console
 void in(const std::string& command)
 {
     out("~ " + command + '\n');
+
+    // Spaghetti parser is go
+    std::vector<ConsoleArg> args;
+    try
+    {
+        args = ParseArgs(command);
+    }
+    catch (const char* error)
+    {
+        out("%s in: %s\n", error, command.c_str());
+    }
+
+    out("<");
+    for (const auto& t : args)
+    {
+        out("(t:");
+        switch (t.type)
+        {
+        case ConsoleArg::NONE:
+            out("n");
+            break;
+        case ConsoleArg::FUNCTION:
+            out("c");
+            break;
+        case ConsoleArg::STRING:
+            out("s");
+            break;
+        case ConsoleArg::INT:
+            out("i");
+            break;
+        case ConsoleArg::FLOAT:
+            out("f");
+            break;
+        }
+        out(",v:'%s'),", t.value.c_str());
+    }
+    out(">\n");
 }
 
 void out(const std::string& fmt, ...)
