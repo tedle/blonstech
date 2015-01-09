0d1696099da299d8c6febffe7d158d43080f06a9
Dominic Bowden <tedle@blon.se>
2015-01-09 06:08:17 +0000

    Diffuse and specular lighting

--- src/shaders/light.frag.glsl
+++ src/shaders/light.frag.glsl
@@ -11,20 +11,52 @@ uniform sampler2D albedo;
 uniform sampler2D normal;
 uniform sampler2D depth;
 
+float refraction_index = 0.5;
+float extinction_coef = 0.5;
+float fresnel_coef = (pow(refraction_index - 1, 2) + pow(extinction_coef, 2)) / (pow(refraction_index + 1, 2) + pow(extinction_coef, 2));
+
+//vec3 light_dir = normalize(vec3(-10.0, -20.0, -5.0));
+vec3 light_dir = normalize(vec3(-10.0, -2.0, -5.0));
+
 void main(void)
 {
+	// World coordinates of the pixel we're rendering
 	vec4 pos = vec4(tex_coord.x,
-					tex_coord.y + 1, // Invert Y because FBOs are top-left origin
+					tex_coord.y + 1, // Invert and shift Y because FBOs are top-left origin
 					texture(depth, tex_coord).r,
 					1.0);
 	pos = inv_proj_view_matrix * (pos * 2.0 - 1.0);
 	pos /= pos.w;
 
-	// Temporarily stop these uniforms from being optimized out
-	vec4 a = texture(normal, tex_coord) * 0.001;
-	vec4 b = texture(albedo, tex_coord) * 0.001;
-	frag_colour = a + b;
-	frag_colour *= 0.0000001;
+	// For directional lights
+	vec4 eye_pos = inv_proj_view_matrix * vec4(0.0, 0.0, 0.0, 1.0);
+	eye_pos /= eye_pos.w;
+	vec3 view_dir = normalize(eye_pos.xyz - pos.xyz);
 
-	frag_colour += pos;
+	vec3 surface_normal = normalize(texture(normal, tex_coord).rgb * 2.0 - 1.0);
+	vec3 half = normalize((-light_dir) + view_dir);
+
+	// Higher exponent used because this is blinn-phong (blinn-phong * 4 ~= phong)
+	float specular = pow(clamp(dot(half, surface_normal), 0.0, 1.0), 20.0);
+
+	// Black out surfaces not facing a light
+	if (dot(surface_normal, -light_dir) < 0.0)
+	{
+		specular = 0;
+	}
+
+	// Linear blend specular with fresnel angle, based on coefficient
+	float fresnel = fresnel_coef + (1 - fresnel_coef) * pow(1.0 - dot(view_dir, half), 5.0);
+	specular *= fresnel;
+
+	// Diffuse lighting (temporary)
+	float diffuse = (dot(-light_dir, surface_normal) + 1.0) / 2.0;
+	// Ambient lighting
+	diffuse = min(1.0, diffuse + 0.1);
+
+	vec3 surface_colour = texture(albedo, tex_coord).rgb;
+	surface_colour *= diffuse;
+	surface_colour += diffuse * specular;
+
+	frag_colour = vec4(surface_colour, 1.0);
 }
